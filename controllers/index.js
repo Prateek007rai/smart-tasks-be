5733969c63b74d3609eb1054755b3386381b9860
Prateek <prateekrai341036@gmail.com>
2026-03-27 13:25:56 +0530

    made controller and saved generated task summary

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const { FAILED_TO_CREATE_TASK, TASK_CREATED_SUCCESSFULLY, 
     FAILED_TO_FETCH_TASKS, TASKS_FETCHED_SUCCESSFULLY,
-     TASK_DELETED_SUCCESSFULLY, FAILED_TO_DELETE_TASK, TASK_NOT_FOUND } = require("../helpers/response");
+     TASK_DELETED_SUCCESSFULLY, FAILED_TO_DELETE_TASK, TASK_NOT_FOUND,
+     FAILED_TO_GENERATE_TASK_SUMMARY, TASK_SUMMARY_GENERATED_SUCCESSFULLY } = require("../helpers/response");
 const taskService = require("../services/task");
-
+const aiService = require("../services/aiService");
 //create task function
 const createTaskController = async(req, res) => {
   try {
@@ -57,10 +58,39 @@ const deleteTaskById = async(req, res) => {
     }
 }
 
-//generate summary of tasks
+//generate summary of task
+const generateTaskSummary = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const task = await taskService.getTaskById(id);
+
+        if(!task){
+            return res.status(404).json({
+                message: TASK_NOT_FOUND,
+            })
+        }
+        //generate summary and save in task
+        const summary = await aiService.generateSummary(task.title);
+        task.generatedSummary = summary;
+        await task.save();
+
+        return res.status(200).json({
+            message: TASK_SUMMARY_GENERATED_SUCCESSFULLY,
+            task,
+        })
+
+    }catch(err){
+        console.log("Error during summary generation : ", err);
+        res.status(500).json({
+            message: FAILED_TO_GENERATE_TASK_SUMMARY,
+        })
+    }
+}
+
 
 module.exports = {
   createTaskController,
   getAllTasksController,
-  deleteTaskById
+  deleteTaskById,
+  generateTaskSummary,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const validateTask = require("../middlewares/validate");
-const { createTaskController, getAllTasksController, deleteTaskById } = require("../controllers");
+const { createTaskController, getAllTasksController, deleteTaskById, generateTaskSummary } = require("../controllers");
 const router = express.Router();
 
 router.post('/create-task', validateTask, createTaskController)
 router.get('/all-tasks', getAllTasksController)
 router.delete('/delete-all-tasks', deleteTaskById)
+router.get('/task-summary/:id', generateTaskSummary)
 
 module.exports = router;
